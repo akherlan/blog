01dfccdfafbb0e5d89aeeb7c421bad3b734e3550
Andi Herlan <herlandi.27@gmail.com>
2018-11-15 05:51:26 +0700

    lebih baik, timeline udah berbentuk

---
+++ static/js/timeline-json.js
@@ -0,0 +1,111 @@
+{
+    "title": {
+        "media": {
+          "url": "https://en.wikipedia.org/wiki/Curriculum_vitae",
+          "credit": "wikipedia"
+        },
+        "text": {
+          "headline": "Riwayat Hidup",
+          "text": "<p>Anda dapat membaca secara ringkas laman yang menjelaskan <a href='about'>tentang saya</a>. Namun Anda juga dapat dengan cepat mengetahui perjalanan lengkap saya melalui <i>timeline</i> berikut.</p>"
+        }
+    },
+    "events": [
+      {
+        "media": {
+          "url": "images/profile.png",
+          "caption": ""
+        },
+        "start_date": {
+          "month": "2",
+          "year": "1995"
+        },
+        "text": {
+          "headline": "Lahir Di Melawi, Kalimantan Barat",
+          "text": "<p>Tumbuh dan hidup berpindah-pindah bersama orangtua hingga usia 6 tahun. Kemudian pindah ke Cirebon dan menetap hingga saat ini.</p>"
+        }
+      },
+      {
+        "background": {
+          "url": "images/wisuda.JPG"
+        },
+        "start_date": {
+          "year": "2013"
+        },
+        "end_date": {
+          "year": "2017",
+          "month": "10"
+        },
+        "text": {
+          "headline": "Menempuh </br> Pendidikan S1 </br> Teknik Sipil",
+          "text": "<p>Memperoleh gelar S.T. dari Universitas Gunadarma.</p>"
+        }
+      },
+      {
+        "media": {
+          "url": "",
+          "caption": "",
+          "credit": ""
+        },
+        "start_date": {
+          "year": "2017",
+          "month" : "12"
+        },
+        "text": {
+          "headline": "Memiliki Kesempatan untuk Bisa Sekolah Lagi",
+          "text": "<p>Saat ini saya sedang menyelesaikan pendidikan S2 saya, masih di bidang teknik sipil dan Universitas yang sama.</p><p>Penelitian yang saya lakukan adalah mengenai perbandingan data klimatologi, khususnya hujan, antara pengamatan dari bumi dengan pengamatan oleh satelit.</p>"
+        }
+      },
+      {
+        "media": {
+          "url": "https://pbs.twimg.com/profile_images/842577890889949185/UeWG1y3I_400x400.jpg",
+          "caption": "Budi Waluyo, Ph.D., mentor Sekolah TOEFL (saat ini <a href='https://sdsafadg.com' target='_blank'>Learn for Future</a>)",
+          "credit": "twitter/<a href='https://twitter.com/01_budi' target='_blank'>01_budi</a>"
+        },
+        "start_date": {
+          "year": "2016"
+        },
+        "text": {
+          "headline": "Murid <a href='https://sdsafadg.com' target='_blank'>Sekolah TOEFL</a> Angkatan 4",
+          "text": "<p>Sekolah TOEFL (Saat ini Learn for Future) merupakan wadah bagi siapapun untuk dapat mempelajari TOEFL secara intensif. Dibimbing dan dibina selama beberapa bulan dengan Pak Budi Waluyo, Ph.D. selaku mentornya.</p><p>Saat ini, Sekolah TOEFL telah menggunakan konsep MOOC yang sedang berkembang. Transformasi ini dibuat karena pendaftar yang semakin banyak sehingga perlu ditangani dengan baik.</p>"
+        }
+      },
+      {
+        "background": {
+          "url": "images/bandung.jpg"
+        },
+        "start_date": {
+          "year": "2016",
+          "month": "12"
+        },
+        "end_date": {
+          "year": "2017",
+          "month": "12"
+        },
+        "text": {
+          "headline": "Terlibat dalam Proyek Konservasi",
+          "text": "<p>Melakukan survei untuk Monitoring/Evaluasi (Monev) Agroforestry serta Konservasi Tanah & Air (KTA) di Daerah Aliran Sungai Citarum-Ciliwung.</p><p>Kegiatan ini merupakan wujud kerjasama antara Jurusan Teknik Sipil Universitas Gunadarma dengan instansi pemerintah, dalam hal ini BPDASHL Citarum-Ciliwung, dalam rangka mengurangi dampak daya rusak air (erosi dan banjir).</p>"
+        }
+      },
+      {
+        "media": {
+          "url": "images/pimpinan_hiama.png",
+          "caption": "HIAMA, Depok"
+        },
+        "background": {
+          "url": "images/hiama.png"
+        },
+        "start_date": {
+          "month": "9",
+          "year": "2014"
+        },
+        "end_date": {
+          "year": "2017",
+          "month": "11"
+        },
+        "text": {
+          "headline": "Membantu Siswa dalam Belajar",
+          "text": "<p>Bersama <a href='https://hiama.com/homeschooling/' target='_blank'>HIAMA</a> Home Schooling, saya sempat merasakan dan menyadari bahwa belajar, dalam konteks apapun itu, tidak akan pernah bisa dihentikan atau berhenti.</p><p>Saya pernah membantu adik-adik siswa SMP dan SMA untuk mengembangkan bakat dan kemampuan mereka dalam sains dan matematika.</p>"
+        }
+      }
+    ]
+}
