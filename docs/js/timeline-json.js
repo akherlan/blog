2e083e8464693b25950108c89272515cb020806c
Andi Herlan <herlandi.27@gmail.com>
2018-11-16 01:44:16 +0700

    publish one

---
+++ docs/js/timeline-json.js
@@ -0,0 +1,149 @@
+{
+    "title": {
+        "media": {
+          "url": "https://en.wikipedia.org/wiki/Curriculum_vitae",
+          "credit": "wikipedia"
+        },
+        "text": {
+          "headline": "Riwayat Hidup",
+          "text": "<p>Anda dapat membaca secara ringkas laman yang menjelaskan <a href='about'>tentang saya</a>. Namun Anda juga dapat dengan cepat mengetahui perjalanan lengkap saya melalui <i>timeline</i> berikut.</p>"
+        }
+    },
+    "events": [
+      {
+        "media": {
+          "url": "https://www.google.com/maps/place/Nanga+Pinoh,+Melawi+Regency,+West+Kalimantan/@-0.4420003,111.661038,6.46z/data=!4m5!3m4!1s0x2e01b45c36bfc0b3:0xbc9a0c056a1cdd5a!8m2!3d-0.3656789!4d111.7376178",
+          "caption": ""
+        },
+        "start_date": {
+          "month": "2",
+          "year": "1995"
+        },
+        "text": {
+          "headline": "Lahir Di Melawi, Kalimantan Barat",
+          "text": "<p>Tumbuh dan hidup berpindah-pindah bersama orangtua hingga usia 6 tahun. Kemudian pindah ke Cirebon dan menetap hingga saat ini.</p>"
+        }
+      },
+      {
+        "background": {
+          "url": "images/wisuda.JPG"
+        },
+        "start_date": {
+          "year": "2013",
+          "month": "8"
+        },
+        "end_date": {
+          "year": "2017",
+          "month": "10"
+        },
+        "text": {
+          "headline": "Menempuh </br> Pendidikan S1 </br> Teknik Sipil",
+          "text": "<p>Memperoleh gelar S.T. dari Universitas Gunadarma.</p>"
+        }
+      },
+      {
+        "media": {
+          "url": "",
+          "caption": "",
+          "credit": ""
+        },
+        "start_date": {
+          "year": "2017",
+          "month" : "12"
+        },
+        "text": {
+          "headline": "Memiliki Kesempatan untuk Bisa Sekolah Lagi",
+          "text": "<p>Saat ini saya sedang menyelesaikan pendidikan S2 saya, masih di bidang teknik sipil dan di perguruan tinggi yang sama.</p><p>Penelitian yang saya lakukan adalah mengenai perbandingan data klimatologi, khususnya hujan, antara pengamatan dari bumi dengan pengamatan oleh satelit.</p>"
+        }
+      },
+      {
+        "media": {
+          "url": "<blockquote class='twitter-tweet data-lang='en'><p lang='in' dir='ltr'>Kalian penasaran nggak sih untuk tahu teknik sukses ala Kak Budi Waluyo <a href='https://twitter.com/01_budi?ref_src=twsrc%5Etfw'>@01_budi</a> pendiri <a href='https://twitter.com/SekolahToefl?ref_src=twsrc%5Etfw'>@SekolahToefl</a>? Yuk simak disini: <a href='https://t.co/cAbgWPFLAR'>https://t.co/cAbgWPFLAR</a> <a href='https://twitter.com/hashtag/SekolahToefl?src=hash&amp;ref_src=twsrc%5Etfw'>#SekolahToefl</a> <a href='https://twitter.com/hashtag/Motivasi?src=hash&amp;ref_src=twsrc%5Etfw'>#Motivasi</a> <a href='https://twitter.com/hashtag/BeasiswaLuarNegeri?src=hash&amp;ref_src=twsrc%5Etfw'>#BeasiswaLuarNegeri</a> # <a href='https://t.co/3uDC3iLUMs'>pic.twitter.com/3uDC3iLUMs</a></p>&mdash; Wijatnika Ika (@Wijatnika45) <a href='https://twitter.com/Wijatnika45/status/1026360262083764224?ref_src=twsrc%5Etfw'>August 6, 2018</a></blockquote><script async src='https://platform.twitter.com/widgets.js' charset='utf-8'></script>",
+          "credit": "twitter/<a href='https://twitter.com/Wijatnika45' target='_blank'>wijatnika45</a>"
+        },
+        "start_date": {
+          "year": "2016"
+        },
+        "text": {
+          "headline": "Murid <a href='https://sdsafadg.com' target='_blank'>Sekolah TOEFL</a>",
+          "text": "<p>Sekolah TOEFL (Saat ini Learn for Future) merupakan wadah bagi siapapun untuk dapat mempelajari TOEFL secara intensif. Saya dibimbing dan dibina selama beberapa bulan oleh Bapak Budi Waluyo, Ph.D. selaku sang mentor.</p>"
+        }
+      },
+      {
+        "background": {
+          "url": "images/bandung.jpg"
+        },
+        "start_date": {
+          "year": "2016",
+          "month": "12"
+        },
+        "end_date": {
+          "year": "2017",
+          "month": "12"
+        },
+        "text": {
+          "headline": "Terlibat dalam Proyek Konservasi",
+          "text": "<p>Melakukan survei untuk Monitoring/Evaluasi (Monev) Agroforestry serta Konservasi Tanah & Air (KTA) di Daerah Aliran Sungai Citarum-Ciliwung.</p><p>Kegiatan ini merupakan wujud kerjasama antara Jurusan Teknik Sipil Universitas Gunadarma dengan instansi pemerintah, dalam hal ini BPDASHL Citarum-Ciliwung, dalam rangka mengurangi dampak daya rusak air (erosi dan banjir).</p>"
+        }
+      },
+      {
+        "media": {
+          "url": "images/hiama/pimpinan.png",
+          "caption": "HIAMA, Depok"
+        },
+        "background": {
+          "url": "images/hiama/gedung.png"
+        },
+        "start_date": {
+          "month": "9",
+          "year": "2014"
+        },
+        "end_date": {
+          "year": "2017",
+          "month": "11"
+        },
+        "text": {
+          "headline": "Membantu Siswa dalam Belajar",
+          "text": "<p>Bersama <a href='https://hiama.com/homeschooling/' target='_blank'>HIAMA</a> Home Schooling, saya sempat merasakan dan menyadari bahwa belajar, dalam konteks apapun itu, tidak akan pernah bisa dihentikan atau berhenti.</p><p>Saya pernah membantu adik-adik siswa SMP dan SMA untuk mengembangkan bakat dan kemampuan mereka dalam sains dan matematika.</p>"
+        }
+      },
+      {
+        "media": {
+          "url": "",
+          "caption": "Foto bersama usai kegiatan English Camp bersama Depok Lingua."
+        },
+        "background": {
+          "url": "images/dling.jpg"
+        },
+        "start_date": {
+          "year": "2015",
+          "month": "2"
+        },
+        "end_date": {
+          "year": "2017",
+          "month": "5"
+        },
+        "text": {
+          "headline": "Belajar Bahasa Bersama Komunitas",
+          "text": "<p>Untuk melatih kemampuan berbahasa asing dan berbicara, saya ikut belajar bersama <b>Depok Lingua</b>, sebuah komunitas bahasa dan budaya di Kota Depok.</p><p>English Camp merupakan salah satu kegiatan yang rutin diselenggarakan. Melalui kegiatan ini, saya dilatih untuk bisa berbicara dalam Bahasa Inggris secara aktif selama kegiatan berlangsung.</p>"
+        }
+      },
+      {
+        "media": {
+          "url": "",
+          "caption": ""
+        },
+        "background": {
+          "url": "images/penulisasik.png"
+        },
+        "start_date": {
+          "month": "10",
+          "year": "2018"
+        },
+        "text": {
+          "headline": "Bergabung dengan Tim Penulis",
+          "text": "<p>Bersama <a href='https://penulisasik.com/' target='_blank'>PenulisAsik.com</a> saya mencoba untuk turut serta membangun ekonomi Indonesia, dengan jalan menaikkan daya saing para pengusaha kecil dan menengah dalam memasarkan produk-produk mereka secara <i>online</i>.</p><p>"
+        }
+      }
+    ]
+}
